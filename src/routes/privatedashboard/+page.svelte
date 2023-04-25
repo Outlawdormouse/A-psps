0b836d33c25295004c6559a97c2adec34016cb9f
PittiDavide <76527290+PittiDavide@users.noreply.github.com>
2023-04-25 23:27:55 +0200

    controllo di non mostrare il proprio account

--- src/routes/privatedashboard/+page.svelte
+++ src/routes/privatedashboard/+page.svelte
@@ -7,13 +7,16 @@
 
 	const db = getDatabase();
 	const usersRef = ref(db, 'users/');
-	let email;
+	let currID;
 	let accounts = []; // json con tutti gli account per inserirli nelle cards
 	let scrollX = 0; // posizione orizzontale dello scorrimento
+	let loading = false
+	let currentUser = [];
+
 	
 	authStore.subscribe((curr) => {
 		console.log('CURR', curr);
-		email = curr?.currentUser?.email;
+		currID = curr?.currentUser?.uid;
 	});
 
 	onValue(usersRef, (snapshot) => {
@@ -21,9 +24,16 @@
 		console.log(data);
 		//for per riscrivere gli elementi nella forma: { id: 1, name: 'What is Slidy?', animalDescription: 'SLIDY – simple configurable carousel component on SvelteJS', src: 'https://picsum.photos/900/900.webp?random=1' },
 		for (const [key, value] of Object.entries(data)) {
-			accounts.push({ id: key, animal: value.animal, animalDescription: value.animal_describtion, animalAge: value.animal_age, animalName: value.animal_name, src: value.animal_image, mail: value.email, name: value.username, gender: value.animal_gender });
+			if(key != currID){
+				accounts.push({ id: key, animal: value.animal, animalDescription: value.animal_describtion, animalAge: value.animal_age, animalName: value.animal_name, src: value.animal_image, mail: value.email, name: value.username, gender: value.animal_gender });
+			}else{
+				currentUser.push({id: key, animal: value.animal, animalDescription: value.animal_describtion, animalAge: value.animal_age, animalName: value.animal_name, src: value.animal_image, mail: value.email, name: value.username, gender: value.animal_gender})
+				console.log("current", currentUser);
+			}
 		}
 		console.log(accounts);
+		loading = true;
+		
 	});
 
 	const slidy_cards = {
@@ -64,13 +74,12 @@
 
 <svelte:window bind:scrollX />
 
-{#if $authStore.currentUser}
+{#if $authStore.currentUser && loading}
 	<div class="container">
-		<div class="navbar">
-			<nav>
+		<div>
+			<nav class = "navbar">
 				<ul>
 					<li><img src = "logo.png" alt="logo" class="logo"/></li>
-					<li><a href="prenotazioni.php">Prenotazioni</a></li>
 					<li><a href="inserimento.php">Account</a></li>
 					<button on:click={authHandlers.logout}>Logout</button>
 				</ul>
@@ -117,27 +126,45 @@ h1 {
 
 .navbar {
     display: flex;
-    align-items: center;
     height: 50px;
-    background-color: #eee;
+    background-color: #cccccc;
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
+	z-index: 1;
+	
+}
+
+.navbar ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-between; /* aggiunta */
+    align-items: center;
+}
+
+.navbar ul li:first-child {
+    margin-right: auto; /* aggiunta */
+}
+
+.navbar ul li:last-child {
+    margin-left: auto; /* aggiunta */
 }
 
 .logo {
     width: 65px;
     height: auto;
     margin-left: 10px;
+
 }
 
 nav ul {
 	list-style: none;
 	margin: 0;
 	padding: 0;
-    display: flex;
-    justify-content: space-between; 
+    display: flex; 
     align-items: center; 
 }
 
@@ -159,10 +186,11 @@ nav button {
 	cursor: pointer;
 	margin-left: 15px; 
 }
+
 @media screen and (min-width: 500px) {
 		:global(#slidy_cards .slidy-ul li) {width: 70vw;}
 	}	
-	@media screen and (min-width: 600px) {
+	@media screen and (min-width: 300px) {
 		:global(#slidy_cards .slidy-ul li) {width: 50vw;}
 	}
 	@media screen and (min-width: 700px) {
@@ -187,9 +215,10 @@ nav button {
 		justify-content: center;
 		height: 100%;
 		border-radius: 1rem;
+		padding: 2rem;
 	}
 	.slide img {
-		max-height: 200px;
+		max-height: 300px;
 		width: 100%;
 		height: 100%;
 		box-sizing: border-box;
